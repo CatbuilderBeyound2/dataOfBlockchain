1fb046f2bf85570c3a7c0bac27570e9cd5b3bf0f
Dec-F <dec_fan@icloud.com>
2018-05-22 18:52:42 +0800

    🚧 总览item

---
+++ src/components/overview/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="content">
+    <overviewItem></overviewItem>
+  </div>
+</template>
+<script>
+import overviewItem from './overview-item';
+export default {
+  components: {
+    overviewItem
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+
---
+++ src/components/overview/overview-item.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="overview-item">
+    <i class="icon" :style="{backgroundImage:`url(${detail.icon})`}"></i>
+    <div class="detail-wrap">
+      <div class="detail-title">
+        {{detail.title}}
+      </div>
+      <div class="detail-count">
+        <countTo :startVal='0' :endVal='detail.count.value' :duration='3000'></countTo>
+        <span class="count-unit">{{detail.count.unit}}</span>
+      </div>
+      <div class="detail-trend">
+
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import countTo from 'vue-count-to';
+export default {
+  components: { countTo },
+  props: {
+    detail: {
+      type: Object,
+      default() {
+        return {
+          icon: 'http://omqz8y2im.bkt.clouddn.com/business-mix-set-2012.png',
+          title: '虚拟币',
+          count: {
+            value: 2000,
+            unit: '万'
+          },
+          trend: '',
+          trendStatus: 1
+        };
+      }
+    }
+  }
+};
+</script>
+<style lang="less" scoped>
+.overview-item {
+  height: 200px;
+  > i,
+  > div {
+    display: inline-block;
+    height: 100%;
+    vertical-align: middle
+  }
+  .icon {
+    width: 100px;
+    background: center no-repeat;
+    background-size: contain
+  }
+
+}
+</style>
+
+
+
