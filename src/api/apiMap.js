f3b52b31f82700d218dd039974f7fe5382b9a8c5
Dec-F <dec_fan@icloud.com>
2018-06-28 16:57:59 +0800

    fix

--- src/api/apiMap.js
+++ src/api/apiMap.js
@@ -1,9 +1,9 @@
 export default {
-  marketSummary: "/marketSummary", //首页数据总览
-  getMarketData: "market/marketData", // 行情数据
+  marketSummary: "/market/marketSummary", //首页数据总览
+  getMarketData: "/market/marketData", // 行情数据
   getTrade: "/trade/trade", //交易平台
   rankings: "rank/rankings", //排行榜
   searchByCoinName: "/market/search",
   searchByTrade: "/trade/search",
-  searchCount: "market/totalNum"
+  searchCount: "/market/totalNum"
 };
