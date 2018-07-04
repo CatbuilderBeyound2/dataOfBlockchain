8afbbcaf67b51b4a1d32dd8a6f590ba5625342f6
xingyue <xingyue@iresearch.com.cn>
2018-07-04 18:37:16 +0800

    7.4晚

--- src/components/table/table3.vue
+++ src/components/table/table3.vue
@@ -1,7 +1,7 @@
 //
 <template>
   <div class="table-3">
-    <el-table :data="tableData"  v-loading="loading" header-row-class-name="table-2-header" @row-click="rowClick" @sort-change="sortChange">
+    <el-table :data="tableData" v-loading="loading" header-row-class-name="table-2-header" @row-click="rowClick" @sort-change="sortChange">
       <el-table-column v-for="(item ,index) in headerData" :sortable="item.sortable?'custom':false" header-align="center" :prop="item.key" :label="item.label" :key="index" :sort-orders='sortOrders' :prop='item.column'>
       </el-table-column>
     </el-table>
@@ -16,6 +16,7 @@ export default {
 };
 </script>
 <style lang="less">
+@import '~@style/var.less';
 .table-3 {
   .el-table tr.table-3-header>th {
     background-color: #dedede;
